45bca3b5b8fd183def9f6709c05777694957f079
grand <grand@LAPTOP-TSVR2PTO>
2017-04-02 19:17:46 -0400

    First checkin

---
+++ helloSpringCore/src/com/springCore/helloWorld/HelloWorldBean.java
@@ -0,0 +1,29 @@
+package com.springCore.helloWorld;
+
+//This is the spring bean class.
+public class HelloWorldBean {
+
+	//private static final HelloWorldBean obj=new HelloWorldBean();  
+	private String msg;
+		
+	private HelloWorldBean(String name){
+		System.out.println(name);
+		}  
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+		
+	public void displayMsg() {
+		System.out.println();
+	}
+	
+	/*public static HelloWorldBean getHelloWorldBean( ){
+	    System.out.println("factory method ");  
+		return new HelloWorldBean();
+	}*/
+}
---
+++ helloSpringCore/src/com/springCore/helloWorld/HelloWorldSpringCoreTest.java
@@ -0,0 +1,33 @@
+package com.springCore.helloWorld;
+
+//import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.FileSystemResource;
+
+
+public class HelloWorldSpringCoreTest {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		
+		//BeanFactory bf = new XmlBeanFactory(new FileSystemResource
+			//	("C:\\Users\\grand\\Desktop\\Pa1 Docs\\Java Workspace\\com.helloSpringCore\\src\\spring-bean.xml"));
+		//HelloWorldBean helloWorldbean2 = (HelloWorldBean) bf.getBean("hello"); //"hello" is the bean id
+		
+		HelloWorldBean helloWorldbean = (HelloWorldBean) context.getBean("hello"); //"hello" is the bean id
+		//HelloWorldBean helloWorldbean1 = (HelloWorldBean) context.getBean("hello");
+
+		helloWorldbean.displayMsg();
+		//helloWorldbean.setMsg("This is my first Spring Program");
+		//if you use singleton as scope then only one object is available even though you instantiated HelloWorldBean twice.
+		//if you use prototype as scope then we can use two objects of HelloWorldBean that we created. (their hashcodes are different as seen)
+		//System.out.println(helloWorldbean.hashCode()); //1st object
+		//System.out.println(helloWorldbean1.hashCode()); //2st object
+		
+
+	}
+
+}
